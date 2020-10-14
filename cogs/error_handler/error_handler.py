48bae05daab8ea161bc8d8281ac527e796c0beaf
Raff <raffsimms23@gmail.com>
2020-10-14 22:57:46 +0100

    base

---
+++ cogs/error_handler/error_handler.py
@@ -0,0 +1,71 @@
+import discord, datetime, sys, traceback, math
+from discord.ext import commands
+
+async def error_embed(ctx, err):
+    embed=discord.Embed(description=err, timestamp=datetime.datetime.utcnow())
+    await ctx.send(embed=embed)
+
+class errorHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        # if command has local error handler, return
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        # get the original exception
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        if isinstance(error, commands.errors.ChannelNotFound):
+            await error_embed(ctx, f"**{str(error)}**")
+
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send("This command is on cooldown, please retry in {}s.".format(math.ceil(error.retry_after)))
+            return
+
+        if isinstance(error, commands.BotMissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(missing)
+            _message = 'I need the **{}** permission(s) to run this command.'.format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.MissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(missing)
+            _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send('This command cannot be used in direct messages.')
+            except discord.Forbidden:
+                pass
+            return
+
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send("You do not have permission to use this command.")
+            return
+
+        elif isinstance(error, commands.BadArgument):
+            if ctx.command.qualified_name == 'reject':  # Check if the command being invoked is 'tag list'
+                await error_embed(ctx, f"**Invalid suggestion ID, please enter a number.**")
+
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+def setup(bot):
+    bot.add_cog(errorHandler(bot))
